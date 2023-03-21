5b47c309db4c73836287bd2f93d4787cc9757162
Vishwajeet Chavan <125532260+JetJeet@users.noreply.github.com>
2023-03-21 22:07:47 +0530

    Create form.js

---
+++ Assignment1B/form.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#btn').click(function(){
+        function getStudentData() {
+            let student = {
+            
+            firstName: $("#firstName").val(),
+            lastName: $("#lastName").val(),
+            
+            };
+            $("#studentForm")[0].reset();
+            
+            return student; }
+        function storeDataToLocalStorage() {
+            if (!localStorage.getItem("student")) {
+                localStorage.setItem("student", JSON.stringify(getStudentData()));
+            } else {
+                localStorage.removeItem("student");
+                localStorage.setItem("student", JSON.stringify(getStudentData()));
+            }
+        }
+        function sendData() {
+            let xhr = new XMLHttpRequest();
+            let data = JSON.stringify(getStudentData());
+            xhr.open("POST", "http://localhost:4000/storedata", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(data);
+        }
+        storeDataToLocalStorage();
+        sendData();
+        window.location.href="display-data.html"
+    })
+})
